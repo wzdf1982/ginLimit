ad71a7c13ad2b72a3dc9312bd90e2a112d57dc4f
DJZhou <dingjun.zhou@gmail.com>
2019-05-05 11:15:42 +0800

    Update limit.go

--- limit.go
+++ limit.go
@@ -13,24 +13,10 @@ func Limit(max int) gin.HandlerFunc {
 	}
 	sema := make(chan struct{}, max)
 	return func(c *gin.Context) {
-		var called, fulled bool
-		defer func() {
-			if called == false && fulled == false {
-				<-sema
-			}
-			if r := recover(); r != nil { // We don't handle panic
-				panic(r)
-			}
-		}()
-
 		select {
 		case sema <- struct{}{}:
 			c.Next()
-			called = true
 			<-sema
-		default:
-			fulled = true
-			c.Status(http.StatusBadGateway)
 		}
 	}
 }
